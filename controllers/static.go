dc69843692e6d9ba7a9beead8be0b9567299cfd2
z-wentao <1728679818@qq.com>
2025-05-14 16:48:51 +0800

    create signup page

--- controllers/static.go
+++ controllers/static.go
@@ -1,6 +1,7 @@
 package controllers
 
 import (
+	"html/template"
 	"net/http"
 
 	"github.com/z-wentao/PhotoShare/views"
@@ -18,4 +19,27 @@ func StaticHandler(tpl views.Template) http.HandlerFunc {
 
 func (static Static) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	static.Template.Execute(w, nil)
+}
+
+func FAQ(tpl views.Template) http.HandlerFunc {
+	questions := []struct {
+		Question string
+		Answer   template.HTML
+	}{
+		{
+			Question: "What is this website about?",
+			Answer:   "As it's named, it help sharing photo with your family and friend.",
+		},
+		{
+			Question: "How to use it",
+			Answer:   "Sign up and log in, upload your photo and share!",
+		},
+		{
+			Question: "How to get support?",
+			Answer:   `Email me - <a href="mailto:support@photoshare.com">support@photoshare.com</a>`,
+		},
+	}
+	return func(w http.ResponseWriter, r *http.Request) {
+		tpl.Execute(w, questions)
+	}
 }
