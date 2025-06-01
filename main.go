82a0a978ec18d7cd90fdacb6b20cdb36d35ff344
z-wentao <1728679818@qq.com>
2025-06-02 01:34:59 +0800

    continue CSRF

--- main.go
+++ main.go
@@ -5,7 +5,6 @@ import (
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
-	"github.com/gorilla/csrf"
 	"github.com/z-wentao/PhotoShare/controllers"
 	"github.com/z-wentao/PhotoShare/models"
 	"github.com/z-wentao/PhotoShare/templates"
@@ -24,12 +23,6 @@ func main() {
 		DB: db,
 	}
 
-	csrfKey := "gFvi45R4fy5xNBlnEeZtQbfAVCYEIAUX"
-	csrfMw := csrf.Protect(
-		[]byte(csrfKey),
-		csrf.Secure(false),
-	)
-
 	usersC := controllers.Users{
 		UserService: &userService,
 	}
@@ -42,6 +35,7 @@ func main() {
 	r.Get("/contact", controllers.StaticHandler(views.Must(views.ParseFS(templates.FS, "contact.gohtml", "tailwind.gohtml"))))
 	r.Get("/faq", controllers.FAQ(views.Must(views.ParseFS(templates.FS, "faq.gohtml", "tailwind.gohtml"))))
 	r.Get("/signup", controllers.StaticHandler(views.Must(views.ParseFS(templates.FS, "signup.gohtml", "tailwind.gohtml"))))
+
 	r.Get("/signin", usersC.SignIn)
 	r.Get("/signup", usersC.New)
 	r.Post("/signup", usersC.Create)
@@ -53,5 +47,6 @@ func main() {
 	})
 
 	fmt.Println("Starting the server on :3000...")
-	http.ListenAndServe(":3000", csrfMw(r))
+
+	http.ListenAndServe(":3000", r)
 }
