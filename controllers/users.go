901abd94337a7a9a4bd209007ad895ab0bba8d59
Zhu Wentao <1728679818@qq.com>
2025-07-27 10:28:52 +0800

    custom template func

--- controllers/users.go
+++ controllers/users.go
@@ -2,9 +2,7 @@ package controllers
 
 import (
 	"fmt"
-	"github.com/gorilla/csrf"
 	"github.com/z-wentao/PhotoShare/models"
-	"html/template"
 	"net/http"
 )
 
@@ -26,11 +24,9 @@ func (u Users) SignIn(w http.ResponseWriter, r *http.Request) {
 
 func (u Users) New(w http.ResponseWriter, r *http.Request) {
 	var data struct {
-		Email     string
-		CSRFField template.HTML
+		Email string
 	}
 	data.Email = r.FormValue("email")
-	data.CSRFField = csrf.TemplateField(r)
 	u.Templates.New.Execute(w, data)
 }
 
--- views/template.go
+++ views/template.go
@@ -13,12 +13,18 @@ type Template struct {
 }
 
 func ParseFS(fs fs.FS, pattern ...string) (Template, error) {
-	htmlTpl, err := template.ParseFS(fs, pattern...)
+	tpl := template.New(pattern[0])
+	tpl = tpl.Funcs(template.FuncMap{
+		"csrfField": func() template.HTML {
+			return `<input type="hidden" />`
+		},
+	})
+	tpl, err := tpl.ParseFS(fs, pattern...)
 	if err != nil {
 		return Template{}, fmt.Errorf("parsing template: %w", err)
 	}
 	return Template{
-		htmlTpl: htmlTpl,
+		htmlTpl: tpl,
 	}, nil
 
 }
