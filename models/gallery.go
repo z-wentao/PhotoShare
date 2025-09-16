c1ffab62b1e6a4c64a395fc0de68684ac1c5e866
Zhu Wentao <1728679818@qq.com>
2025-09-16 16:30:25 +0800

    wrap the logic of pic existed checking

--- models/gallery.go
+++ models/gallery.go
@@ -4,6 +4,8 @@ import (
 	"database/sql"
 	"errors"
 	"fmt"
+	"io/fs"
+	"os"
 	"path/filepath"
 	"strings"
 )
@@ -151,3 +153,19 @@ func (service GalleryService) galleryDir(id int) string {
     }
     return filepath.Join(imagesDir, fmt.Sprintf("gallery-%d", id))
 }
+
+func (service *GalleryService) Image(galleryID int, filename string) (Image, error) {
+    imagePath := filepath.Join(service.galleryDir(galleryID), filename)
+    _, err := os.Stat(imagePath)
+    if err != nil {
+	if errors.Is(err, fs.ErrNotExist) {
+	    return Image{}, ErrNotFound
+	}
+	return Image{}, fmt.Errorf("quering for image: %w", err)
+    }
+    return Image {
+	Filename: filename,
+	GalleryID: galleryID,
+	Path: imagePath,
+    }, nil
+} 
